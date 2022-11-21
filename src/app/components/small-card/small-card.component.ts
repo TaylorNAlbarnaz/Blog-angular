17c4468f9eb19f8e0a22b6f6f5215b18099758e4
Taylor N. Albarnaz <veloxgods@gmail.com>
2022-11-21 15:43:56 -0300

    Cards agora são modulares

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'small-card',
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
-
+  @Input()
+  cardPhoto: string = "https://ichef.bbci.co.uk/news/640/cpsprodpb/133CB/production/_103759787_gettyimages-949493748.jpg";
+  
+  @Input()
+  cardDescription: string = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem ipsum mollitia magni assumenda repellendus temporibus fugiat fuga, a repellat.";
+  
+  @Input()
+  cardDate: string = "14 de setembro";
+  
   constructor() { }
 
   ngOnInit(): void {
